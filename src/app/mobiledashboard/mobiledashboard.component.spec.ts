584a422d51b49c0fa3f64209d46fd9821589c9eb
A231049 <simthembile.yuku@standardbank.co.za>
2019-09-06 12:14:54 +0200

    Fixing failing tests

--- src/app/mobiledashboard/mobiledashboard.component.spec.ts
+++ src/app/mobiledashboard/mobiledashboard.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MobiledashboardComponent } from './mobiledashboard.component';
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {AngularFireDatabase} from "@angular/fire/database";
 
 describe('MobiledashboardComponent', () => {
   let component: MobiledashboardComponent;
   let fixture: ComponentFixture<MobiledashboardComponent>;
 
+  let subscribeMock = { subscribe: jasmine.createSpy(('subscribe')).and.returnValue([]) };
+  let valueChangesMock = {
+    valueChanges: jasmine.createSpy('valueChanges').and.returnValue(subscribeMock)
+  };
+  let db = {
+    list: jasmine.createSpy('list').and.returnValue(valueChangesMock)
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MobiledashboardComponent ]
+      declarations: [ MobiledashboardComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        MobiledashboardComponent, {
+          provide: AngularFireDatabase,
+          useValue: db
+        }
+      ],
     })
     .compileComponents();
   }));
